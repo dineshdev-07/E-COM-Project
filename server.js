01f498d788b5ed4e3cf93a7c69ab7263a327c2a0
Dinesh <dineshdevtech@gmail.com>
2026-05-30 21:45:30 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -28,30 +28,12 @@ const __dirname = path.dirname(__filename);
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://ecommerce-frontend-fawn-three.vercel.app",
-  );
-
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(
+  cors({
+    origin: "https://ecommerce-frontend-fawn-three.vercel.app",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
