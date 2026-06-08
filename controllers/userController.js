60f47c82a7d77cf3d3b61995135e31d099057b44
Dinesh <dineshdevtech@gmail.com>
2026-06-08 19:55:10 +0530

    'UPDATE'

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,11 @@ import asyncHandler from "express-async-handler";
 
 let otpStore = {};
 
-// Initialize Nodemailer Transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
+  requireTLS: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
