3cb925b7e24d0ea13f970a1211fbf5dae7a61029
Dinesh <dineshdevtech@gmail.com>
2026-06-15 18:35:52 +0530

    debug wishlist

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -2,20 +2,24 @@ import User from "../models/userModel.js";
 import Product from "../models/Product.js";
 
 export const getWishlist = async (req, res) => {
-  console.log("GET WISHLIST HIT");
   try {
-    const user = await User.findById(req.user._id).populate(
-      "wishlist",
-      "name price discountedPrice images brand quantity views salesCount expiryDate",
-    );
+    console.log("GET WISHLIST HIT");
+    console.log("USER ID:", req.user._id);
+    console.log("BEFORE POPULATE");
+
+    const user = await User.findById(req.user._id).populate("wishlist");
+
+    console.log("AFTER POPULATE", user);
+
     res.json(user.wishlist || []);
   } catch (err) {
-  console.error("WISHLIST ERROR:", err);
-  res.status(500).json({
-    message: "Failed to fetch wishlist",
-    error: err.message,
-  });
-}
+    console.error("WISHLIST ERROR:", err);
+
+    res.status(500).json({
+      message: "Failed to fetch wishlist",
+      error: err.message,
+    });
+  }
 };
 
 export const toggleWishlist = async (req, res) => {
